c17bd329c35d619ab4df4726e91b8bccf8deac40
Christopher Bunkers <Christopher.bunkers@gmail.com>
2015-12-29 10:38:06 -0600

    Add math folder and fix syntax error in address directory.

--- week-4/address/my_solution.rb
+++ week-4/address/my_solution.rb
@@ -4,7 +4,7 @@
 
 # Your Solution Below
 def make_address(street, city, state, zip)
-  puts "You live at " + street.to_s + ", in the beautiful city of " + city + ", " + state + ". Your zip is " + zip.to_s + "."
+"You live at " + street.to_s + ", in the beautiful city of " + city + ", " + state + ". Your zip is " + zip.to_s + "."
 end
 
 make_address("633 Folsom St.","San Francisco","CA","94107")
