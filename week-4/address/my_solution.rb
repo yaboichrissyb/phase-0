12a2f81489935630208328dcbcf713aad39cfba4
Christopher Bunkers <Christopher.bunkers@gmail.com>
2015-12-29 10:24:00 -0600

    add format_address_spec.rb and my_solution.rb

---
+++ week-4/address/my_solution.rb
@@ -0,0 +1,10 @@
+# Format an Address
+
+# I worked on this challenge by myself.
+
+# Your Solution Below
+def make_address(street, city, state, zip)
+  puts "You live at " + street.to_s + ", in the beautiful city of " + city + ", " + state + ". Your zip is " + zip.to_s + "."
+end
+
+make_address("633 Folsom St.","San Francisco","CA","94107")
